59aff3ce39b04b730e9de202933001c8ef8410b2
Sigma <jskolska@edu.cdv.pl>
2024-04-07 10:53:09 +0200

    moan /raptor

--- Assets/Scripts/Player/PointManager.cs
+++ Assets/Scripts/Player/PointManager.cs
@@ -1,15 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PointManager : MonoBehaviour
 {
     private int score;
+    [SerializeField] private TextMeshProUGUI pointText;
+
+    private void Awake()
+    {
+        score = 0;
+        pointText.text = score.ToString();
+    }
 
     public void AddPoint()
     {
         score += 1;
-        Debug.Log(score);
+        pointText.text = score.ToString();
     }
 }
